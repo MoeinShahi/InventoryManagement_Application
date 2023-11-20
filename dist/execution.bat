cefb51ceb424e237811ee57958f3ab3226dbfed0
Moein Shahi <shahy4779@gmail.com>
2023-11-20 16:53:06 +0100

    Importing project code

---
+++ dist/execution.bat
@@ -0,0 +1,2 @@
+java -jar Inventory.Management.jar
+pause
---
+++ src/inventory/management/InventoryManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package inventory.management;
+
+/**
+ *
+ * @author shahy
+ */
+public class InventoryManagement {
+    public static void main(String[] args){
+        new MainJFrame().setVisible(true);
+    }
+    
+}
